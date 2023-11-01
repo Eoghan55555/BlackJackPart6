f999f31f194528039b5f183737c97f99d5d1ae01
Eoghan55555 <123965051+Eoghan55555@users.noreply.github.com>
2023-11-01 12:53:56 +0000

    Add files via upload
    More progress done to the program.cs file

---
+++ BlackJack_CA - Part6/BlackJack_CA/Betting.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack_CA
+{
+    internal class Betting
+    {
+        public enum Result { BlackJack, Win, Draw, Loss } //Enum for each scenario in BlackJack
+        
+        private double _bettingChips;
+        private string _name;
+        public double BetValue 
+        { 
+            get;
+            set; 
+        }
+        public double BettingChips
+        {
+      
+            get;
+            set;
+        }
+        public string Name
+        {
+            get;
+            set;
+        }
+        public Betting()
+        {
+
+        }
+        //Way of getting the remaing chips after inputting the BetValue.
+        public double BetCalc() //Calculate 
+        {
+            if (BetValue >= BettingChips) //To avoid going into negative chips
+            {
+                BetValue = BettingChips;
+                BettingChips = 0;
+            }
+            else if (BetValue < BettingChips)
+            {
+                BettingChips = BettingChips - BetValue;
+            }
+            return BettingChips;
+        }
+        public void BetWinning(Result match)
+        {
+            if (match == Result.BlackJack) //Scenario when player gets BlackJack
+            {
+                BetValue = BetValue * 2.5;
+                BettingChips = BettingChips + BetValue;
+            }
+            else if (match == Result.Win) //Scenario when player wins
+            {
+                BetValue = BetValue * 2;
+                BettingChips = BettingChips + BetValue;
+            }
+            else if (match == Result.Draw) //Scenario when player draws
+            {
+                BettingChips += BetValue;
+            }
+            else if (match == Result.Loss) //Scenario when player loses or busts
+            {
+                BettingChips -= BetValue;
+            }
+        }
+        public string GetName() //Gets the username of the player
+        {
+            Console.Write("Enter Name: ");
+            _name = Console.ReadLine();
+            return _name;
+        }
+        public double GetTotalBettingChips() //Gets the total amount of chips
+        {
+            Console.Write("Enter the amount of betting chips you want to have: ");
+            _bettingChips = double.Parse(Console.ReadLine());
+            return _bettingChips;
+        }
+        public override string ToString() //Test to see if the fields have values
+        {
+            return _name + " your total betting chips is " + _bettingChips;
+        }
+    }
+}
---
+++ BlackJack_CA - Part6/BlackJack_CA/Program.cs
@@ -0,0 +1,118 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace BlackJack_CA
+{
+    internal class Program
+    {
+        static Card Player1 = new Card(); 
+        static Card Dealer = new Card();
+        static Betting _Player1 = new Betting();
+        static int p_num_ofcards = 0;//Count the num of cards a player would have
+        static int d_num_ofcards = 0;//Count the num of cards a dealer would have
+        static void Main(string[] args)
+        {
+
+
+            //string card = Player1.GetCard();
+            //Player1.GetCardValue(card);
+            //_Player1.GetName();
+            //_Player1.GetTotalBettingChips();
+            //Console.WriteLine(_Player1.ToString());
+            
+            Player1.DeckReshuffle();//Shuffles Deck
+
+            //_Player1.GetName();//Start of the Final Draft
+            //_Player1.GetTotalBettingChips();
+            int playerhandvalue=PlayBlackJack();
+            int dealerhandvalue =DealerPlays();
+            GetResult(playerhandvalue, dealerhandvalue);
+        }
+        static int PlayBlackJack()
+        {
+            //Variables
+            string input = "";
+            string lowercaseinput="";
+
+            int TotalHandValue = 0;
+;
+            //First Draw
+            string card = Player1.GetCard();//Gets the card as a string
+            int cardValue = Player1.GetCardValue(card);//Turns the string value into a number
+            p_num_ofcards++;
+            Console.WriteLine($"\nYou have drawn {card} which is worth {cardValue}");
+
+            TotalHandValue += cardValue;
+            
+            while (lowercaseinput != "stay")
+            {
+                card= Player1.GetCard();
+                cardValue = Player1.GetCardValue(card);
+                p_num_ofcards++;
+                Console.WriteLine($"\nYou have drawn {card} which is worth {cardValue}");
+                
+                TotalHandValue+= cardValue;
+                Console.WriteLine($"Total is now {TotalHandValue}\n");
+
+                if (TotalHandValue >= 21)
+                {
+                    break;
+                }
+
+                Console.Write("Do you want to stay or hit: ");
+                input=Console.ReadLine();
+                lowercaseinput = input.ToLower();
+
+
+            } 
+            
+            return TotalHandValue;
+        }
+
+        static int DealerPlays()
+        {
+            int totalHandValue = 0;
+
+            while (totalHandValue < 17)
+            {
+                string card = Dealer.GetCard();//Gets the card as a string
+                int cardValue = Dealer.GetCardValue(card);//Turns the string value into a number
+                d_num_ofcards++;
+                Console.WriteLine($"\nDealer has drawn {card} which is worth {cardValue}");
+                totalHandValue+= cardValue;
+                Console.WriteLine($"Dealer's Total is now {totalHandValue}\n");
+            }
+            return totalHandValue;
+        }
+        static void GetResult(int playerhand,int dealerhand)
+        {
+            //Loss Scenarios
+            if (playerhand > 21) //Player Busts
+            {
+                _Player1.BetWinning(Betting.Result.Loss); 
+            }
+            else if (playerhand < dealerhand && dealerhand >= 21) //Dealer is below 22 and has more than the player
+            {
+                _Player1.BetWinning(Betting.Result.Loss);
+            }
+            //Win Scenarios
+            else if (dealerhand > 21) //Player is Below 22 and Dealer Busts
+            {
+                _Player1.BetWinning(Betting.Result.Win);
+            }
+            else if (playerhand > dealerhand) //Player has more than Dealer
+            {
+                _Player1.BetWinning(Betting.Result.Win);
+            }
+            //Draw Scenarios
+            else if (playerhand == dealerhand) //Player and Dealer has the same amount
+            {
+                _Player1.BetWinning(Betting.Result.Draw);
+            }
+            //Player gets BlackJack
+            else if (playerhand == 21) //Player gets a natural 21
+            {
+                _Player1.BetWinning(Betting.Result.BlackJack);
+            }
+        }
+    }
+}
